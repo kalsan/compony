788a94bb9bb534982782cbc535ed567743ed7781
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2022-12-15 13:27:37 +0100

    Document compony's root module

--- lib/compony.rb
+++ lib/compony.rb
@@ -1,16 +1,35 @@
+# @api description
+# Root module, containing confguration and pure helpers. For
+# the setters, create an initializer `config/initializers/compony.rb` and call
+# them from there.
 module Compony
   ##########=====-------
   # Configuration writers
   ##########=====-------
 
+  # Setter for the global button component class. This allows you to implement a
+  # custom button component and have all Compony button helpers use your custom
+  # button component instead of {Compony::Components::Button}.
+  # @param button_component_class [Class] Your custom button component class (inherit from {Compony::Components::Button} or {Compony::Component})
   def self.button_component_class=(button_component_class)
     @button_component_class = button_component_class
   end
 
+  # Setter for the global form helper class. This allows you to implement a
+  # custom form helper with additional features and have all form components use
+  # it instead of the default {Compony::ModelFields::FormHelper}.
   def self.form_helper_class=(form_helper_class)
     @form_helper_class = form_helper_class
   end
 
+  # Setter for the name of the Rails `before_action` that should be called to
+  # ensure that users are authenticated before accessing the component. For
+  # instance, implement a method `def enforce_authentication` in your
+  # `ApplicationController`. In the method, make sure the user has a session and
+  # redirect to the login page if they don't. <br> The action must be accessible
+  # by {ComponyController} and the easiest way to achieve this is to implement
+  # the action in your `ApplicationController`. If this is never called,
+  # authentication is disabled.
   def self.authentication_before_action=(authentication_before_action)
     @authentication_before_action = authentication_before_action.to_sym
   end
@@ -19,18 +38,24 @@ module Compony
   # Configuration readers
   ##########=====-------
 
+  # Getter for the global button component class.
+  # @see Compony#button_component_class=
   def self.button_component_class
     @button_component_class ||= Components::Button
     @button_component_class = const_get(@button_component_class) if @button_component_class.is_a?(String)
     return @button_component_class
   end
 
+  # Getter for the global form helper class.
+  # @see Compony#form_helper_class=
   def self.form_helper_class
     @form_helper_class ||= ModelFields::FormHelper
     @form_helper_class = const_get(@form_helper_class) if @form_helper_class.is_a?(String)
     return @form_helper_class
   end
 
+  # Getter for the name of the Rails `before_action` that enforces authentication.
+  # @see Compony#authentication_before_action=
   def self.authentication_before_action
     @authentication_before_action
   end
@@ -40,6 +65,10 @@ module Compony
   ##########=====-------
 
   # Given a component and a family/model, this returns the matching component class if any
+  # @param comp_name_or_cst [Constant,String,Symbol] The component that should be loaded, for instance `ShowForAll`, `'ShowForAll'` or `:show_for_all`
+  # @param model_or_family_name_or_cst [Constant,String,Symbol,ApplicationRecord] Either the family that contains the requested component,
+  #                                    or an instance implementing `model_name` from which the family name is auto-generated. Examples:
+  #                                    `Users`, `'Users'`, `:users`, `User.first`
   def self.comp_class_for(comp_name_or_cst, model_or_family_name_or_cst)
     family_name_or_cst = model_or_family_name_or_cst
     if model_or_family_name_or_cst.respond_to?(:model_name)
@@ -48,19 +77,26 @@ module Compony
     return ::Components.const_get(family_name_or_cst.to_s.camelize).const_get(comp_name_or_cst.to_s.camelize)
   end
 
-  # Given a component and a family, this returns the name of the Rails URL helper returning the path to this component.
-  # Optionally can pass a name for extra standalone configs.
+  # Given a component and a family, this returns the name of the Rails URL helper returning the path to this component.<br>
+  # The parameters are the same as for {Compony#rails_action_name}.<br>
+  # Example usage: `send("#{path_helper_name(:index, :users)}_url)`
+  # @see Compony#rails_action_name
   def self.path_helper_name(...)
     "#{rails_action_name(...)}_comp"
   end
 
-  # Given a component and a family, this returns the name of the ComponyController action for this component.
+  # Given a component and a family, this returns the name of the ComponyController action for this component.<br>
   # Optionally can pass a name for extra standalone configs.
+  # @param comp_name [String,Symbol] Name of the component the action points to.
+  # @param family_name [String,Symbol] Name of the family the action points to.
+  # @param name [String,Symbol] If referring to an extra standalone entrypoint, specify its name using this param.
+  # @todo Allow passing models as family
   def self.rails_action_name(comp_name, family_name, name = nil)
     [name.presence, comp_name, family_name].compact.join('_')
   end
 
   # Given a component and a family/model, this instanciates and returns a button component.
+  # @todo document params
   def self.button_comp(comp_name_or_cst, model_or_family_name_or_cst, **kwargs)
     model = model_or_family_name_or_cst.respond_to?(:model_name) ? model_or_family_name_or_cst : nil
     target_comp = Compony.comp_class_for(comp_name_or_cst, model_or_family_name_or_cst).new(data: model)
@@ -68,6 +104,7 @@ module Compony
   end
 
   # Given a component instance, this instanciates and returns a button component.
+  # @todo document params
   def self.button_comp_for(target_comp_instance, model = nil, label_format: :long, params: {}, **kwargs)
     options = {
       label:   target_comp_instance.label(model, format: label_format),
@@ -79,7 +116,8 @@ module Compony
     return Compony.button_component_class.new(**options.symbolize_keys)
   end
 
-  # Raw method for producing a button
+  # Raw method for producing a {Compony::Components::Button} (or a custom implementation if {Compony#button_component_class=} was used).<br/>
+  # All arguments are passed to the button's initializer.
   def self.button(...)
     button_component_class.new(...)
   end
