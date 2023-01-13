55d75b5e8f2b927c061afff32fb0d138387fe5ea
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-01-13 10:17:20 +0100

    Form: Fail early when a field group is missing

--- lib/compony/components/form.rb
+++ lib/compony/components/form.rb
@@ -100,6 +100,9 @@ module Compony
       # See also notes for `with_form_helper`.
       def field(name, **kwargs)
         fail("The `field` method may only be called inside `form_fields` for #{inspect}.") unless @form_helper
+        unless @form_helper.form.object.field_groups[field_group_key]
+          fail("No field group #{field_group_key.inspect} found for #{@form_helper.form.object.class}")
+        end
         unless @form_helper.form.object.field_groups[field_group_key].fields.include?(name.to_sym)
           fail("Component #{self} is operating on field group #{field_group_key} which does not include requested field #{name.to_sym.inspect}.")
         end
