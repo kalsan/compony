14221f8f96dc1b47c12c6735aa2b6d682d1d1f22
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-01-18 12:03:40 +0100

    Properly configure load paths

--- lib/compony/engine.rb
+++ lib/compony/engine.rb
@@ -1,6 +1,16 @@
 module Compony
   class Engine < Rails::Engine
-    initializer 'compony.initialize' do
+    initializer "compony.configure_eager_load_paths", before: :load_environment_hook, group: :all do
+      # Allow app/components/foo/bar.rb to define constants Components::Foo::Bar and make sure components are eager loaded (needed for route generation etc.)
+      Rails.application.config.eager_load_paths.delete("#{Rails.root}/app/components")
+      Rails.application.config.eager_load_paths.unshift("#{Rails.root}/app")
+
+      # Prevent *.rb files in assets and views directories to be loaded
+      Rails.autoloaders.main.ignore("#{Rails.root}/app/assets")
+      Rails.autoloaders.main.ignore("#{Rails.root}/app/views")
+    end
+
+    initializer 'compony.controller_mixin' do
       ActiveSupport.on_load :action_controller_base do
         include Compony::ControllerMixin
       end
