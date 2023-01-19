6ec264736c85c5c2252e2d32f5582bc19564e774
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-01-19 10:51:07 +0100

    Satisfy rubocop

--- lib/compony/engine.rb
+++ lib/compony/engine.rb
@@ -1,13 +1,13 @@
 module Compony
   class Engine < Rails::Engine
-    initializer "compony.configure_eager_load_paths", before: :load_environment_hook, group: :all do
+    initializer 'compony.configure_eager_load_paths', before: :load_environment_hook, group: :all do
       # Allow app/components/foo/bar.rb to define constants Components::Foo::Bar and make sure components are eager loaded (needed for route generation etc.)
-      Rails.application.config.eager_load_paths.delete("#{Rails.root}/app/components")
-      Rails.application.config.eager_load_paths.unshift("#{Rails.root}/app")
+      Rails.application.config.eager_load_paths.delete(Rails.root.join('app', 'components').to_s)
+      Rails.application.config.eager_load_paths.unshift(Rails.root.join('app').to_s)
 
       # Prevent *.rb files in assets and views directories to be loaded
-      Rails.autoloaders.main.ignore("#{Rails.root}/app/assets")
-      Rails.autoloaders.main.ignore("#{Rails.root}/app/views")
+      Rails.autoloaders.main.ignore(Rails.root.join('app', 'assets').to_s)
+      Rails.autoloaders.main.ignore(Rails.root.join('app', 'views').to_s)
     end
 
     initializer 'compony.controller_mixin' do
