9b6c16e5b93ef31d46f7eb7d9bb1d66501b5f0dc
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-01-19 11:39:44 +0100

    Create feasibility feature in ModelMixin

--- lib/compony/model_mixin.rb
+++ lib/compony/model_mixin.rb
@@ -3,8 +3,10 @@ module Compony
     extend ActiveSupport::Concern
 
     included do
+      # TODO: The following class attribute initializations will likely not work for STI/inherited models as subclasses will probably influence superclasses.
       class_attribute :fields, default: {}
       class_attribute :field_groups, default: {}
+      class_attribute :feasibility_preventions, default: {}
     end
 
     class_methods do
@@ -27,6 +29,41 @@ module Compony
           field_groups[name] = new_field_group
         end
       end
+
+      # DSL method, part of the Feasibility feature
+      # Block must return `false` if the action should be prevented.
+      def prevent(action_name, message, &block)
+        feasibility_preventions[action_name.to_sym] ||= []
+        feasibility_preventions[action_name.to_sym] << MethodAccessibleHash.new.merge({ action_name:, message:, block: })
+      end
+    end
+
+    # Retrieves feasibility for the given instance
+    # Calling this with an invalid action name will always return true.
+    def feasible?(action_name, recompute: false)
+      action_name = action_name.to_sym
+      @feasibility_messages ||= {}
+      # Abort if check has already run and recompute is false
+      if @feasibility_messages[action_name].nil? || recompute
+        # Compute feasibility and gather messages
+        @feasibility_messages[action_name] = []
+        feasibility_preventions[action_name]&.each do |prevention|
+          if instance_exec(&prevention.block)
+            @feasibility_messages[action_name] << prevention.message
+          end
+        end
+      end
+      return @feasibility_messages[action_name].none?
+    end
+
+    def feasibility_messages(action_name)
+      action_name = action_name.to_sym
+      feasible? if @feasibility_messages[action_name].nil? # If feasibility check hasn't been performed yet for this action, perform it now
+      return @feasibility_messages[action_name]
+    end
+
+    def full_feasibility_messages(action_name)
+      return feasibility_messages(action_name).join(', ').capitalize
     end
   end
 end
