1161d42bd19ed7faf69f7535888c06a498d8c63d
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-06-07 16:57:26 +0200

    Run rubocop

--- lib/compony/model_fields/url.rb
+++ lib/compony/model_fields/url.rb
@@ -4,7 +4,7 @@ module Compony
       def value_for(data, controller: nil, **_)
         return transform_and_join(data.send(@name), controller:) do |el|
           fail('Must pass controller to generate the link to the link.') unless controller
-          return controller.helpers.link_to(el, el, target: '_blank')
+          return controller.helpers.link_to(el, el, target: '_blank', rel: 'noopener')
         end
       end
     end
