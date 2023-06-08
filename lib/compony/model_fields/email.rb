04b63e95cd89845bdec9882b598d2633c005fa74
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-06-08 10:31:10 +0200

    Correctly handle nils in email and url fields

--- lib/compony/model_fields/email.rb
+++ lib/compony/model_fields/email.rb
@@ -4,6 +4,7 @@ module Compony
       def value_for(data, controller: nil, **_)
         return transform_and_join(data.send(@name), controller:) do |el|
           fail('Must pass controller to generate the link to the email.') unless controller
+          return nil unless el
           return controller.helpers.mail_to(el)
         end
       end
