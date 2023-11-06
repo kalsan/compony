f1149c66c585a856e949d17b003e01e21a2117b5
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-11-06 15:26:48 +0100

    Switch to edge

--- lib/compony/version.rb
+++ lib/compony/version.rb
@@ -2,9 +2,9 @@ module Compony
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 0
+    PATCH = 1
 
-    EDGE = false
+    EDGE = true
 
     LABEL = [MAJOR, MINOR, PATCH, EDGE ? 'edge' : nil].compact.join('.')
   end
